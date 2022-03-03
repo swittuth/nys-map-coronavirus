f6a8609ff0e6f50973d52247e8ecfdd9160454e2
swittuth <swittuthcoding@gmail.com>
2022-03-02 21:43:15 -0500

    updated color for map and imported covid data into script file

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,28 @@ const map = new mapboxgl.Map({
 });
 let hoveredStateId = null;
 
+// promise that contains data on covid
+const covid_data_promise = fetch("covid_data.json").then(e => e.json());
+const state_list = ['Albany', 'Allegany', 'Bronx', 'Broome', 'Cattaraugus', 'Cayuga', 'Chautauqua', 'Chemung', 'Chenango', 
+                    'Clinton', 'Columbia', 'Cortland', 'Delaware', 'Dutchess', 'Erie', 'Essex', 'Franklin', 'Fulton', 'Genesee', 
+                    'Greene', 'Hamilton', 'Herkimer', 'Jefferson', 'Kings', 'Lewis', 'Livingston', 'Madison', 'Monroe', 'Montgomery', 
+                    'Nassau', 'New', 'Niagara', 'Oneida', 'Onondaga', 'Ontario', 'Orange', 'Orleans', 'Oswego', 'Otsego', 
+                    'Putnam', 'Queens', 'Rensselaer', 'Richmond', 'Rockland', 'St Lawrence', 'Saratoga', 'Schenectady', 'Schoharie', 
+                    'Schuyler', 'Seneca', 'Steuben', 'Suffolk', 'Sullivan', 'Tioga', 'Tompkins', 'Ulster', 'Warren', 'Washington', 
+                    'Wayne', 'Westchester', 'Wyoming', 'Yates']
+
 map.on('load', () => {
     map.addSource('nys-counties', {
         type: "geojson",
         data: "./nys_counties_id.geojson"
     });
 
+    const matchExpression = ['match', ['get', 'NAME']];
+
+    for (let i = 0; i < state_list.length; i++){
+        matchExpression.push(state_list[i], `rgba(${Math.floor(Math.random() * 257)}, ${Math.floor(Math.random() * 257)}, ${Math.floor(Math.random() * 257)}, ${Math.floor(Math.random() * 257)})`)
+    }
+    matchExpression.push('rgba(0, 0, 0, 0)');
 
     map.addLayer({
         'id': 'nys-counties-fill-layer',
@@ -20,7 +36,7 @@ map.on('load', () => {
         'source': 'nys-counties',
         'layout': {},
         'paint': {
-            'fill-color': '#627BC1',
+            'fill-color': matchExpression,
             'fill-opacity': [
                 'case',
                 ['boolean', ['feature-state', 'hover'], false], 
@@ -82,9 +98,3 @@ map.on('load', () => {
     });
 
 });
-
-// ["point", "lngLat", "originalEvent", 
-// "features", "type", "_defaultPrevented", "target"]
-
-//["NAME", "ABBREV", "GNIS_ID", "FIPS_CODE", 
-// "SWIS", "NYSP_ZONE", "NYC", "CALC_SQ_MI", "Shape_Leng", "Shape_Area"]
